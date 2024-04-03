92ce71a6202c16f1e9ff27bfbee647fac7f3c331
kanowfy <nguyendung2002hl@gmail.com>
2024-04-03 21:18:47 +0700

    setup sqlc with schema and basic queries

---
+++ query.sql
@@ -0,0 +1,55 @@
+-- name: GetAllUsers :many
+SELECT * FROM users;
+
+-- name: GetUserByID :one
+SELECT * FROM users
+WHERE id = $1;
+
+-- name: GetUserByUsername :one
+SELECT * FROM users
+WHERE username = $1;
+
+-- name: ActivateUser :exec
+UPDATE users SET activated = TRUE
+WHERE id = $1;
+
+-- name: UpdateUserProfilePicture :exec
+UPDATE users SET profile_picture = $2
+WHERE id = $1;
+
+-- name: UpdateUserPassword :exec
+UPDATE users SET hashed_password = $2
+WHERE id = $1;
+
+-- name: DeleteUserByID :exec
+DELETE FROM users WHERE id = $1;
+
+-- name: CreateUser :one
+INSERT INTO users (
+    username, hashed_password, email, first_name, last_name, profile_picture
+) VALUES (
+    $1, $2, $3, $4, $5, $6
+)
+RETURNING *;
+
+-- name: GetAllProjects :many
+SELECT * FROM projects;
+
+-- name: GetProjectByID :one
+SELECT * FROM projects
+WHERE id = $1;
+
+-- name: UpdateProjectFund :exec
+UPDATE projects SET current_amount = current_amount + $2
+WHERE id = $1;
+
+-- name: DeleteProjectByID :exec
+DELETE FROM projects WHERE id = $1;
+
+-- name: CreateProject :one
+INSERT INTO projects (
+    user_id, title, description, cover_picture, goal_amount, country, province, end_date
+) VALUES (
+    $1, $2, $3, $4, $5, $6, $7, $8
+)
+RETURNING *;
---
+++ schema.sql
@@ -0,0 +1,55 @@
+CREATE TYPE user_type AS ENUM (
+    'regular',
+    'escrow'
+);
+
+CREATE TABLE IF NOT EXISTS users (
+	id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+	username varchar(64) UNIQUE NOT NULL,
+	hashed_password text NOT NULL,
+	email varchar(255) UNIQUE NOT NULL,
+	first_name varchar(64),
+	last_name varchar(64),
+	profile_picture text,
+	activated boolean NOT NULL DEFAULT false,
+	user_type user_type NOT NULL DEFAULT 'regular'
+);
+
+CREATE TABLE IF NOT EXISTS projects (
+	id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+	user_id uuid NOT NULL REFERENCES users(id),
+	title text NOT NULL,
+	description text NOT NULL,
+	cover_picture text NOT NULL,
+	goal_amount decimal(10,2) NOT NULL,
+	current_amount decimal(10,2) NOT NULL DEFAULT 0.00,
+	country varchar(64) NOT NULL,
+	province varchar(64) NOT NULL,
+	start_date timestamptz NOT NULL DEFAULT NOW(),
+	end_date timestamptz NOT NULL,
+	is_active boolean NOT NULL DEFAULT TRUE
+);
+
+CREATE TABLE IF NOT EXISTS backings (
+	id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+	project_id uuid NOT NULL REFERENCES projects(id),
+	backer_id uuid NOT NULL REFERENCES users(id),
+	amount decimal(10,2) NOT NULL,
+	backing_date timestamptz NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS project_updates (
+	id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+	project_id uuid NOT NULL REFERENCES projects(id),
+	description text NOT NULL,
+	update_date timestamptz NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS project_comments (
+	id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+	project_id uuid NOT NULL REFERENCES projects(id),
+	backer_id uuid NOT NULL REFERENCES users(id),
+	parent_comment_id uuid REFERENCES project_comments(id),
+	content text NOT NULL,
+	commented_at timestamptz NOT NULL DEFAULT NOW()
+);
